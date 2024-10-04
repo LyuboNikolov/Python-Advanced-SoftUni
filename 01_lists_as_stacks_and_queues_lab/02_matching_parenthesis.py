77daa4ba926162776dfef82d550caa527169ede8
LyuboNikolov <155641067+LyuboNikolov@users.noreply.github.com>
2024-10-04 20:57:14 +0200

    Delete 01_lists_as_stacks_and_queues_lab/02_matching_parenthesis.py

--- 01_lists_as_stacks_and_queues_lab/02_matching_parenthesis.py
+++
@@ -1,9 +0,0 @@
-expression = input()
-parenthesis = []
-
-for index in range(len(expression)):
-    if expression[index] == "(":
-        parenthesis.append(index)
-    elif expression[index] == ")":
-        start_index = parenthesis.pop()
-        print(expression[start_index:index + 1])
