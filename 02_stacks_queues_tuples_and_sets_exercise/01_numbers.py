e61f75aa07df6aca06c0176b4436b6f951e53d33
LyuboNikolov <155641067+LyuboNikolov@users.noreply.github.com>
2024-10-20 20:10:48 +0200

    Add files via upload

--- 02_stacks_queues_tuples_and_sets_exercise/01_numbers.py
+++ 02_stacks_queues_tuples_and_sets_exercise/01_numbers.py
@@ -1,21 +1,20 @@
 first_set = set(int(x) for x in input().split())
 second_set = set(int(x) for x in input().split())
 
+functions = {
+    "Add First": lambda x: [first_set.add(int(x)) for x in data],
+    "Add Second": lambda x: [second_set.add(int(x)) for x in data],
+    "Remove First": lambda x: [first_set.discard(int(x)) for x in data],
+    "Remove Second": lambda x: [second_set.discard(int(x)) for x in data],
+    "Check Subset": lambda x: print(first_set.issubset(second_set) or second_set.issubset(first_set))
+}
+
 for _ in range(int(input())):
     first_word, second_word, *data = input().split()
 
     command = first_word + " " + second_word
 
-    if command == "Add First":
-        [first_set.add(int(x)) for x in data]
-    elif command == "Add Second":
-        [second_set.add(int(x)) for x in data]
-    elif command == "Remove First":
-        [first_set.discard(int(x)) for x in data]
-    elif command == "Remove Second":
-        [second_set.discard(int(x)) for x in data]
-    elif command == "Check Subset":
-        print(first_set.issubset(second_set) or second_set.issubset(first_set))
+    functions[command](data)
 
 print(*sorted(first_set), sep=", ")
 print(*sorted(second_set), sep=", ")
