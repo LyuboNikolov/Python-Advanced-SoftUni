602da4a82917facc9f3816667faed45c3676a367
LyuboNikolov <155641067+LyuboNikolov@users.noreply.github.com>
2024-10-23 23:26:24 +0200

    Add files via upload

--- 02_stacks_queues_tuples_and_sets_exercise/03_milkshakes.py
+++ 02_stacks_queues_tuples_and_sets_exercise/03_milkshakes.py
@@ -5,21 +5,23 @@ milk = deque(int(x) for x in input().split(", "))
 
 chocolate_milkshakes = 0
 
-while chocolate_milkshakes < 5 and chocolate and milk:
+while chocolate_milkshakes != 5 and chocolate and milk:
     current_choco = chocolate.pop()
-    if current_choco <= 0:
-        continue
-
     current_milk = milk.popleft()
-    if current_milk <= 0:
+
+    if current_choco <= 0 and current_milk <= 0:
+        continue
+    elif current_choco <= 0:
+        milk.appendleft(current_milk)
+        continue
+    elif current_milk <= 0:
         chocolate.append(current_choco)
         continue
 
     if current_choco == current_milk:
         chocolate_milkshakes += 1
-
     else:
-        chocolate.append(current_choco + 5)
+        chocolate.append(current_choco - 5)
         milk.append(current_milk)
 
 if chocolate_milkshakes == 5:
@@ -27,14 +29,5 @@ if chocolate_milkshakes == 5:
 else:
     print("Not enough milkshakes.")
 
-if chocolate:
-    print(f"Chocolate:", end=" ")
-    print(*chocolate, sep=", ")
-else:
-    print("Chocolate: empty")
-
-if milk:
-    print(f"Milk:", end=" ")
-    print(*milk, sep=", ")
-else:
-    print("Milk: empty")
+print(f"Chocolate: {', '.join(str(c) for c in chocolate) or 'empty'}")
+print(f"Milk: {', '.join(str(m) for m in milk) or 'empty'}")
