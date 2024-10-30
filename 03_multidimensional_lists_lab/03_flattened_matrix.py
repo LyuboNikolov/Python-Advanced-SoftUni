29ede61a3a50076851f167671bb316a5c3e87a4e
LyuboNikolov <155641067+LyuboNikolov@users.noreply.github.com>
2024-10-30 23:46:09 +0100

    Delete 03_multidimensional_lists_lab/03_flattened_matrix.py

--- 03_multidimensional_lists_lab/03_flattened_matrix.py
+++
@@ -1,8 +0,0 @@
-flattened_matrix = []
-
-for _ in range(int(input())):
-    current_row = [int(x) for x in input().split(", ")]
-    for num in current_row:
-        flattened_matrix.append(num)
-
-print(flattened_matrix)
