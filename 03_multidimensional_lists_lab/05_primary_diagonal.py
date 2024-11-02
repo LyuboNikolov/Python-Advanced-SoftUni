09452d96b1d1f287bf23983e331f7f338a77edd4
LyuboNikolov <155641067+LyuboNikolov@users.noreply.github.com>
2024-11-02 20:32:55 +0100

    Add files via upload

--- 03_multidimensional_lists_lab/05_primary_diagonal.py
+++ 03_multidimensional_lists_lab/05_primary_diagonal.py
@@ -1,14 +1,12 @@
 matrix_size = int(input())
 matrix = []
-diagonal_sum = 0
 
 for _ in range(matrix_size):
     current_row = [int(x) for x in input().split()]
     matrix.append(current_row)
 
-for i in range(matrix_size):
-    for j in range(matrix_size):
-        if i == j:
-            diagonal_sum += matrix[i][j]
+diagonal_sum = 0
+for row_index in range(matrix_size):
+    diagonal_sum += matrix[row_index][row_index]
 
 print(diagonal_sum)
