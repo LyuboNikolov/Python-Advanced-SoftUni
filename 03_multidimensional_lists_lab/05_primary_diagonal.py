ebee091b4a1635c4f2ed414c08adb6e5326acdfb
LyuboNikolov <155641067+LyuboNikolov@users.noreply.github.com>
2024-11-02 20:37:28 +0100

    Add files via upload

--- 03_multidimensional_lists_lab/05_primary_diagonal.py
+++ 03_multidimensional_lists_lab/05_primary_diagonal.py
@@ -6,7 +6,7 @@ for _ in range(matrix_size):
     matrix.append(current_row)
 
 diagonal_sum = 0
-for row_index in range(matrix_size):
-    diagonal_sum += matrix[row_index][row_index]
+for index in range(matrix_size):
+    diagonal_sum += matrix[index][index]
 
 print(diagonal_sum)
