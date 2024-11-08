fa711d3ea7e92af4c40f6646b1dcbdd1c01b4eb2
LyuboNikolov <155641067+LyuboNikolov@users.noreply.github.com>
2024-11-08 22:23:17 +0100

    Add files via upload

---
+++ 03_multidimensional_lists_exercise_1/08_bombs.py
@@ -0,0 +1,33 @@
+size = int(input())
+matrix = [[int(x) for x in input().split()] for _ in range(size)]
+
+coordinates = ((int(x) for x in c.split(",")) for c in input().split())
+
+directions = (
+    (-1, 0),
+    (1, 0),
+    (0, -1),
+    (0, 1),
+    (-1, -1),
+    (-1, 1),
+    (1, -1),
+    (1, 1),
+    (0, 0)
+)
+
+for row, col in coordinates:
+    if matrix[row][col] <= 0:
+        continue
+
+    for x, y in directions:
+        r, c = row + x, col + y
+
+        if 0 <= r < size and 0 <= c < size:
+            matrix[r][c] -= matrix[row][col] if matrix[r][c] > 0 else 0
+
+alive_cells = [num for row in range(size) for num in matrix[row] if num > 0]
+
+print(f"Alive cells: {len(alive_cells)}")
+print(f"Sum: {sum(alive_cells)}")
+
+[print(*row) for row in matrix]
